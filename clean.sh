81ac97541dfbdd406b65983d62fa82baf1d56a3e
chuck <chucktilbury@gmail.com>
2025-03-30 11:11:44 -0500

    WIP

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf ast/ parser/ scanner/ keywords.txt non-terminals.txt tokens.txt
+rm -rf ast/ parser/ scanner/ keywords.txt non-terminals.txt tokens.txt srules.txt terminals.txt
